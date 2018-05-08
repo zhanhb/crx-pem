fa84deb470541471345dba341917c7a4daf82bde
zhanhb <zhanhb88@gmail.com>
2018-05-08 16:41:57 +0800

    Merge onload, page onchange

--- webapp/site.js
+++ webapp/site.js
@@ -1,18 +1,16 @@
 angular.module('mainApp', ['ngResource', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']).controller('main', function ($scope, $resource) {
+    function load() {
+        main.get({page: pagination.page - 1, limit: pagination.limit}, function (data) {
+            delete data.page;
+            angular.extend(pagination, data);
+        });
+    }
+
     var pagination = $scope.pagination = {
         page: 1,
-        total: 100,
-        totalPages: 0,
-        pageChanged() {
-            main.get({page: pagination.page - 1, limit: 100}, function (data) {
-                delete data.page;
-                angular.extend(pagination, data);
-            });
-        }
+        limit: 100,
+        pageChanged: load
     };
     var main = $resource('/');
-    main.get({offset: 0, limit: 100}, function (data) {
-        delete data.page;
-        angular.extend(pagination, data);
-    });
+    load();
 });
